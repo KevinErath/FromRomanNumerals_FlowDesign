183831573a9be89334f1ff33a00fb3db3700feeb
Kevin Erath <KevinErath@users.noreply.github.com>
2015-06-27 23:02:35 +0200

    Initial checkin

---
+++ FromRomanNumerals.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+internal static class FromRomanNumerals
+{
+  public static int Convert(string romanNumber)
+  {
+    var romanNumerals = SplitRomanNumerals(romanNumber);
+    var decimals = ConvertToDecimal(romanNumerals);
+    var negatedDecimals = NegateWhenLarger(decimals);
+    return Sum(negatedDecimals);
+  }
+
+  private static char[] SplitRomanNumerals(string romanNumber)
+  {
+    return romanNumber.ToCharArray();
+  }
+
+  private static int[] ConvertToDecimal(IEnumerable<char> romanNumerals)
+  {
+    var mapping = new Dictionary<char, int> {{'I', 1}, {'V', 5},
+                           {'X', 10}, {'L', 50}, {'C', 100},
+                           {'D', 500}, {'M', 1000}};
+    return romanNumerals.Select(x => mapping[x]).ToArray();
+  }
+
+  private static int[] NegateWhenLarger(int[] decimals)
+  {
+    var result = new int[decimals.Length];
+    for (var i = 0; i < decimals.Length; i++)
+    {
+      if (i < decimals.Length - 1 && decimals[i] < decimals[i + 1])
+      {
+        result[i] = -decimals[i];
+      }
+      else
+      {
+        result[i] = decimals[i];
+      }
+    }
+    return result;
+  }
+
+  private static int Sum(int[] decimals)
+  {
+    return decimals.Sum();
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FromRomanNumerals_FlowDesign
+
+A simple solution which solves the kata 'From roman numerals' with the help of Flow Design.
+
+Read more about this implementation on:
+http://www.code-whisperer.de/2015/04/19/plan
